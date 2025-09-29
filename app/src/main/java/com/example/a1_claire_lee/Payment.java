318036471714058369725fa256e10cfb26c1bf29
Claire Lee <jclairelee2@gmail.com>
2025-09-29 14:10:02 -0700

    add payment class

---
+++ app/src/main/java/com/example/a1_claire_lee/Payment.java
@@ -0,0 +1,12 @@
+package com.example.a1_claire_lee;
+
+public class Payment {
+    public final double hours;
+    public final double rate;
+    public final double basePay;
+    public final double overtimePay;
+    public final double totalPay;
+    public final double tax;
+    public final long timestamp;
+
+}
