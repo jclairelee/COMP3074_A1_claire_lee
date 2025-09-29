034f5afae7a005c37f5c364d67ae78fdebed84fe
Claire Lee <jclairelee2@gmail.com>
2025-09-29 15:41:56 -0700

    Add Payment constructors with and without timestamp

--- app/src/main/java/com/example/a1_claire_lee/Payment.java
+++ app/src/main/java/com/example/a1_claire_lee/Payment.java
@@ -9,4 +9,17 @@ public class Payment {
     public final double tax;
     public final long timestamp;
 
+    public Payment(double hours, double rate, double basePay, double overtimePay, double totalPay, double tax, long timestamp) {
+        this.hours = hours;
+        this.rate = rate;
+        this.basePay = basePay;
+        this.overtimePay = overtimePay;
+        this.totalPay = totalPay;
+        this.tax = tax;
+        this.timestamp = timestamp;
+    }
+
+    public Payment(double hours, double rate, double basePay, double overtimePay, double totalPay, double tax) {
+        this(hours, rate, basePay, overtimePay, totalPay, tax, System.currentTimeMillis());
+    }
 }
