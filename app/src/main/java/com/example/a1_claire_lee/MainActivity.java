3c165f03e66b59da9d1b62291bf7ebd93479816a
Claire Lee <jclairelee2@gmail.com>
2025-09-29 13:43:39 -0700

    wire MainActivity inputs

---
+++ app/src/main/java/com/example/a1_claire_lee/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.a1_claire_lee;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etHours;
+    private EditText etRate;
+    private TextView tvPay;
+    private TextView tvOvertimePay;
+    private TextView tvTotalPay;
+    private TextView tvTax;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etHours = findViewById(R.id.etHours);
+        etRate = findViewById(R.id.etRate);
+        tvPay = findViewById(R.id.tvPay);
+        tvOvertimePay = findViewById(R.id.tvOvertimePay);
+        tvTotalPay = findViewById(R.id.tvTotalPay);
+        tvTax = findViewById(R.id.tvTax);
+
+        Button btnCalculate = findViewById(R.id.btnCalculate);
+
+    }
+
+}
